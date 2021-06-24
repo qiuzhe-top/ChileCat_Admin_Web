263bac0b38edcfd27d79a6d15ca7348b651f71a8
qiuzhe <2810201146@qq.com>
2021-06-24 22:15:20 +0800

    V2.0

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,7 +5,7 @@ import { getToken } from '@/utils/auth'
 
 // create an axios instance
 const service = axios.create({
-  baseURL: 'http://localhost:8000/api/',//process.env.VUE_APP_BASE_API, // url = base url + request url
+  baseURL: 'http://47.102.215.230:8000/api/',//process.env.VUE_APP_BASE_API, // url = base url + request url
   // baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 10000 // request timeout
