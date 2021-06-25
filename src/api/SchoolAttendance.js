a642e69dee937f5f7e1385dc8bd53099cc2b9e6d
qiuzhe <2810201146@qq.com>
2021-06-25 18:22:51 +0800

    V2.0 优化

--- src/api/SchoolAttendance.js
+++ src/api/SchoolAttendance.js
@@ -1,8 +1,8 @@
 import request from '@/utils/request'
 
 const api_url = '/school_attendance'
-export const in_zaoqian_excel_url ='http://localhost:8000/api' + api_url + '/in_zaoqian_excel'
-export const out_excel_data ='http://localhost:8000/api' + api_url + '/out_data'
+export const in_zaoqian_excel_url ='http://47.102.215.230:8000/api' + api_url + '/in_zaoqian_excel'
+export const out_excel_data ='http://47.102.215.230:8000/api' + api_url + '/out_data'
 // export const in_zaoqian_excel_url = process.env.VUE_APP_BASE_API + api_url + '/in_zaoqian_excel'
 
 // 获取任务
