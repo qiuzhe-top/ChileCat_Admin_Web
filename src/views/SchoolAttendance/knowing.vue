bc6199a0c117c52d0f617408964fddaafdce72a0
qiuzhe <2810201146@qq.com>
2021-09-26 15:01:17 +0800

    优化班表

--- src/views/SchoolAttendance/knowing.vue
+++ src/views/SchoolAttendance/knowing.vue
@@ -15,39 +15,64 @@
           :key="index"
           class="roster_box"
         >
-          <h3>{{ floor.title }}</h3>
+          <div class="building">{{ floor.title }}</div>
 
           <div
             v-for="(layer, layer_index) in floor.layer_list"
             :key="layer_index"
             class="layer"
           >
-            <h4>{{ layer.title }}</h4>
+            <div class="floor">{{ layer.title }}</div>
 
             <div
               v-for="(user, user_index) in layer.user"
               :key="user_index"
               class="layer"
             >
-              {{ user.username }} {{ user.name }} {{ user.tel }}
-              <el-button
-                v-show="is_show_button"
-                size="mini"
-                icon="el-icon-close"
-                @click="remove_user(layer, user_index)"
-              />
-            </div>
-            <div v-show="is_show_button" class="search_box">
-              <el-row :gutter="10">
-                <el-col :xs="24" :md="8">
-                  <el-input
-                    v-model="layer.user_object.username"
-                    size="small"
-                    placeholder="学号"
+              <el-row>
+                <el-col :span="10">
+                  {{ user.grade }} {{ user.username }} {{ user.name }}
+                  {{ user.tel }} {{ user.type }}
+                </el-col>
+                <el-col :span="6">
+                  <el-autocomplete
+                    v-show="is_show_button"
+
+                    v-model="user.type"
+                    class="inline-input"
+                    :fetch-suggestions="querySearch"
+                    placeholder="请输入内容"
                   />
                 </el-col>
+                <el-col :span="6">
+                  <el-button
+                    v-show="is_show_button"
+                    size="mini"
+                    icon="el-icon-close"
+                    @click="remove_user(layer, user_index)"
+                  />
+                </el-col>
+              </el-row>
+            </div>
 
-                <el-col :xs="24" :md="8">
+            <!-- <div v-show="is_show_button" class="search_box"> -->
+            <el-row v-show="is_show_button" :gutter="10">
+              <el-col :xs="5" :md="8">
+                <el-input
+                  v-model="layer.user_object.username"
+                  size="small"
+                  placeholder="学号"
+                />
+              </el-col>
+              <el-col :xs="5" :md="8">
+                <el-button
+                  v-show="is_show_button"
+                  size="small"
+                  icon="el-icon-search"
+                  @click="search_user(layer)"
+                >添加</el-button>
+              </el-col>
+              <!-- <el-col :xs="24" :md="8">
                   <el-input
                     v-model="layer.user_object.name"
                     size="small"
@@ -62,21 +87,16 @@
                     placeholder="电话"
                   />
                 </el-col>
-              </el-row>
-            </div>
+              </el-row> -->
+            </el-row>
+            <!-- </div> -->
 
-            <el-button
-              v-show="is_show_button"
-              size="small"
-              icon="el-icon-search"
-              @click="search_user(layer)"
-            >搜索</el-button>
-            <el-button
+            <!-- <el-button
               v-show="is_show_button"
               size="small"
               icon="el-icon-circle-plus-outline"
               @click="add_user(layer)"
-            >添加</el-button>
+            >添加</el-button> -->
           </div>
         </div>
       </template>
@@ -101,11 +121,34 @@ export default {
       user_list_str: '',
       excel_url: '',
       // 是否显示排班功能按钮
-      is_show_button: true
+      is_show_button: true,
+      options: [
+        {
+          value: 1,
+          label: '黄金糕'
+        },
+        {
+          value: 2,
+          label: '双皮奶'
+        },
+        {
+          value: 3,
+          label: '蚵仔煎'
+        }
+      ]
     }
   },
 
   methods: {
+    querySearch(queryString, cb) {
+      var results = [
+        { 'value': '统计审核' },
+        { 'value': '随查' },
+        { 'value': '检查' }
+      ]
+      // 调用 callback 返回建议列表的数据
+      cb(results)
+    },
     // 显示班表简易版
     simple_information() {
       this.$data.is_show_button = !this.$data.is_show_button
@@ -122,7 +165,7 @@ export default {
       }
     },
 
-    // 添加多个用户
+    // // 添加多个用户
     add_user_list_str() {
       const str = this.$data.user_list_str
       if (str.length < 1) return
@@ -139,6 +182,7 @@ export default {
 
       console.log(user_list)
     },
+
     // 排班 删除用户
     remove_user(layer, index) {
       layer.user.splice(index, 1)
@@ -151,6 +195,7 @@ export default {
         })
         .then(res => {
           layer.user_object = res.data
+          this.add_user(layer)
         })
 
       clearTimeout(this.timeout)
@@ -159,4 +204,21 @@ export default {
   }
 }
 </script>
-<style></style>
+<style lang="scss" scoped>
+.roster_box {
+  // background-color: aquamarine;
+  // text-align: center;
+}
+.building {
+    margin-top: 10px;
+  font-size: 18px;
+  font-weight: bold;
+}
+.floor {
+  font-size: 15px;
+  font-weight: bold;
+}
+.input_type {
+  width: 100px;
+}
+</style>
