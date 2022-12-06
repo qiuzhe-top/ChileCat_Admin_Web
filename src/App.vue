1898638c211b66375f939551781353b5bac450a9
ZouYang <2810201146@qq.com>
2022-12-06 12:57:01 +0800

    测试版本置灰

--- src/App.vue
+++ src/App.vue
@@ -35,6 +35,13 @@ export default {
 }
 </script>
 <style lang="scss">
+	html{
+		-webkit-filter : grayscale(100%);
+		-moz-filter: grayscale(100%);
+		-o-filter: grayscale(100%);
+		filter: grayscale(100%);
+		filter: progid:DXImageTransform.Microsoft.BasicImage(grayscale=1);
+	}
 // import
 .el-col {
   border-radius: 4px;
